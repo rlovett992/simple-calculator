ccf671bf103caea1f3c453d71d6610aaaadda76e
Robert Lovett <rlovett66@icloud.com>
2026-06-02 21:56:04 -0400

    v2 - looping

--- calculator.py
+++ calculator.py
@@ -1,28 +1,39 @@
+def menu():
+    print("Addition: 1")
+    print("Subtraction: 2")
+    print("Mutiplication: 3")
+    print("Division: 4")
+    print("Exit: 0")
+    return
+
 print("Simple Calculator")
 
-# Get input
-num1 = float(input("Enter first number: "))
-operator = input("Enter operator (+, -, *, /): ")
-num2 = float(input("Enter second number: "))
-
-# Perform calculation
-if operator == "+":
-    result = num1 + num2
-
-elif operator == "-":
-    result = num1 - num2
-
-elif operator == "*":
-    result = num1 * num2
-
-elif operator == "/":
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        result = "Error: Cannot divide by zero."
-
-else:
-    result = "Invalid operator."
-
-# Display result
-print("Result:", result)
+while True:
+    menu()
+    selection = input("Make your selection: ")
+    
+    if selection == "0":
+        break
+    elif selection == "1":
+        num1 = float(input("What is the first number: "))
+        num2 = float(input("What is the second number: "))
+        total = num1 + num2
+        print(total)
+    elif selection == "2":
+        num1 = float(input("What is the first number: "))
+        num2 = float(input("What is the second number: "))
+        total = num1 - num2
+        print(total)
+    elif selection == "3":
+        num1 = float(input("What is the first number: "))
+        num2 = float(input("What is the second number: "))
+        total = num1 * num2
+        print(total)
+    elif selection == "4":
+        num1 = float(input("What is the first number: "))
+        num2 = float(input("What is the second number: "))
+        if num2 == 0:
+            print("Error: division by zero")
+        else:
+            total = num1 / num2
+            print(total)
